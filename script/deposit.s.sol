2e65c2c9d4a0a1fbe7dd2deadcf2681e9e76bfc4
Meek Msaki <98189596+mmsaki@users.noreply.github.com>
2024-12-02 07:53:01 -0600

    📰 new project ef audit

--- script/deposit.s.sol
+++ script/deposit.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.6.11;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {DepositContract} from "../src/deposit.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    DepositContract public deposit;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        deposit = new DepositContract();
 
         vm.stopBroadcast();
     }
---
+++ test/deposit.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.11;
+
+pragma experimental ABIEncoderV2;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DepositContract} from "../src/deposit.sol";
+
+contract DepositTest is Test {
+    DepositContract public deposit;
+
+    function setUp() public {
+        deposit = new DepositContract();
+        vm.deal(address(this), 64 ether);
+    }
+
+    function testFuzz_Deposit(
+
+        ) public payable {
+        bytes memory p = hex"92fbe9f4761625673fe21ce9db5c975d40f1f64e1b554ebb5bd71d0a5de13d30d8ed894cceb5487d193b9defb23eb941";
+        bytes memory w = hex"010000000000000000000000fe948cb2122fdd87baf43dce8afa254b1242c199";
+        bytes memory s = abi.encodePacked(
+            bytes32(0x86d64339aca21ba35a7762fd8e40b8b56618f61546455efe2a6073954eff1884),
+            bytes32(0x7e927fce031bd90fb77bfa88b46966ac0c20217ab40846bea8120306102b6b26),
+            bytes32(0x108efe39acf5ead479e21f8686c120510b83ef83e7b1c43cf2ecaeca69fb6f56)
+            );
+        bytes32 d = 0x08345126fa802d58b2afb567bee5879c9d19dd228f099f5b271944bb64f1bacd;
+
+        console.log(block.timestamp, "0.0 Contract balance:", address(this).balance);
+        deposit.deposit{value: 32 ether}(p,w,s,d);
+
+        // console.log(block.timestamp, "0.1 Depositing 31 ETH...");
+        // console.log(block.timestamp, "0.2 Contract balance:", address(this).balance);
+        // deposit.deposit{value: 1 ether}(p,w,s,d);
+
+        // console.log(block.timestamp, "0.3 Depositing 1 ETH...");
+        // console.log(block.timestamp, "0.4 Contract balance:", address(this).balance);
+        console.log("0.5 Exit. ⚠️");
+    }
+}
